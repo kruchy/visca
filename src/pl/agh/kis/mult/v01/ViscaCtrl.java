aba7153d656c76ba66f51869d5f211bfcc557f89
kruchy <smerfy12>
2015-12-08 07:36:12 +0100

    czytanie z seriala

--- src/pl/agh/kis/mult/v01/ViscaCtrl.java
+++ src/pl/agh/kis/mult/v01/ViscaCtrl.java
@@ -1,30 +1,26 @@
 package pl.agh.kis.mult.v01;
 
-import java.util.*;
-
 import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 import pl.agh.kis.mult.v01.command.*;
 
-public class ViscaCtrl implements SerialPortEventListener {
+public class ViscaCtrl {
     private SerialPort serialPort = null;
-    private String definingMacro = null;
-    private HashMap<String, ArrayList<String>> macroMap = new HashMap<>();
     private ChainCommand chainCommand;
-    private String macroName;
-    public boolean canWriteToPort() {
-        return serialPort.isOpened();
-    }
 
     public ViscaCtrl() {
         serialPort = new SerialPort("com3");
         try {
             serialPort.openPort();
             serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-            serialPort.addEventListener(this);
-            serialPort.setEventsMask(SerialPort.MASK_RXCHAR);
+            serialPort.addEventListener(serialPortEvent -> {
+                try {
+                    String msg = serialPort.readHexString(serialPortEvent.getEventValue());
+                    System.out.println("Serial received: " + msg);
+                } catch (SerialPortException e) {
+                    System.err.println("Error during reading serial port. " + e);
+                }
+            }, SerialPort.MASK_RXCHAR);
         } catch (SerialPortException e) {
             System.err.println("Cannot write to port");
         }
@@ -38,7 +34,6 @@ public class ViscaCtrl implements SerialPortEventListener {
         chainCommand.setNext(new SetAddressCommand());
         chainCommand.setNext(new TeleZoomCommand());
         chainCommand.setNext(new WideZoomCommand());
-        chainCommand.setNext(new WaitCommand());
         try {
             executeCommand("set");
         } catch (SerialPortException e) {
@@ -57,109 +52,19 @@ public class ViscaCtrl implements SerialPortEventListener {
     public void executeCommand(String commandString)
             throws SerialPortException, UnknownCommandException, InterruptedException {
 
-        if (commandString.contains("macro-start")) {
-            startMacro(commandString);
-        } else if (commandString.contains("macro-stop")) {
-            stopMacro();
-        } else if (commandString.contains("macro-execute")) {
-            executeMacro(commandString);
-        } else if (commandString.contains("macro-list")) {
-            listMacros();
-        }
-        else if(commandString.contains("set cam"))
+        if(commandString.contains("close"))
         {
-            setNewCam(commandString);
+                System.exit(0);
         }
-        else if (definingMacro != null) {
-            putNewMacro(commandString);
-        } else if (definingMacro == null)
-        {
-            executeSingleCommand(commandString);
-        } else if(commandString.contains("close"))
-        {
-            System.exit(0);
-        }
+        else executeSingleCommand(commandString);
+
     }
 
-    private void setInterval(String commandString) throws UnknownCommandException, InterruptedException, SerialPortException {
-        String[] command = commandString.split(" ");
-        ChainCommand.waitInterval = Integer.valueOf(command[1]);
-        putNewMacro(command[0]);
-    }
 
-    private void setNewCam(String commandString) {
-        String[] command = commandString.split(" ");
-        chainCommand.setCam(Integer.valueOf(command[2]));
-    }
-
-    private void listMacros() {
-        for (String entry : macroMap.keySet())
-        {
-          System.out.println("MACRO "+ entry);
-            Arrays.asList(macroMap.get(entry)).stream().forEach(System.out::println);
-        }
-    }
 
     private void executeSingleCommand(String commandString) throws UnknownCommandException, SerialPortException {
-        if(commandString.contains("wait"))
-        {
-            int interval = Integer.valueOf(commandString.split(" ")[1]);
-            try {
-                Thread.sleep(interval);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        chainCommand.execute(commandString.split(" ")[0], serialPort);
+               chainCommand.execute(commandString, serialPort);
     }
 
-    private void putNewMacro(String commandString) {
-        if (macroMap.get(definingMacro) == null) {
-            ArrayList<String> list = new ArrayList<>();
-            list.add(commandString);
-            macroMap.put(definingMacro, list);
-        } else {
-            macroMap.get(definingMacro).add(commandString);
-        }
-    }
 
-    private void executeMacro(String commandString) throws UnknownCommandException, SerialPortException, InterruptedException {
-        macroName = commandString.split("\\s+")[1];
-        if (macroMap.containsKey(macroName)) {
-
-            System.out.println("Started executing macro " + macroName);
-            ArrayList<String> commandList = macroMap.get(macroName);
-            Iterator<String> commandIterator = commandList.iterator();
-            while (commandIterator.hasNext()) {
-                executeSingleCommand(commandIterator.next());
-                Thread.sleep(1000);
-                executeSingleCommand("stop");
-            }
-        }
-    }
-
-    private void stopMacro() {
-        System.out.println("Stopped defining macro");
-        definingMacro = null;
-    }
-
-    private void startMacro(String commandString) {
-        macroName = commandString.split("\\s+")[1];
-        System.out.println("Started defining macro " + macroName);
-        macroMap.put(macroName, new ArrayList<>());
-        definingMacro = macroName;
-    }
-
-    @Override
-    public void serialEvent(SerialPortEvent serialPortEvent) {
-        if (serialPortEvent.isRXCHAR() && serialPortEvent.getEventValue() == 10) {
-            try {
-                byte[] response = serialPort.readBytes(10);
-                System.out.println(response);
-            } catch (SerialPortException e) {
-                System.err.println("Error receiving bytes from port");
-            }
-        }
-
-    }
 }
--- src/pl/agh/kis/mult/v01/command/ChainCommand.java
+++ src/pl/agh/kis/mult/v01/command/ChainCommand.java
@@ -15,28 +15,12 @@ public abstract class ChainCommand {
 
     protected ChainCommand next;
     public static byte currentAddress = (byte) 0x81;
-    protected boolean daisyChaining = false;
 
     protected byte tail = (byte) 0xFF;
-    protected static byte[] heads = {(byte) 0x81, (byte) 0x82, (byte) 0x83, (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87};
-
-    public boolean isDaisyChaining() {
-        return daisyChaining;
-    }
-
-    public void setDaisyChaining(boolean daisyChaining) {
-        this.daisyChaining = daisyChaining;
-    }
 
     public ChainCommand(String commandName) {
         this.commandName = commandName;
     }
-
-    public ChainCommand getNext() {
-        return next;
-    }
-
-    public String secondParam;
 
     public void setNext(ChainCommand next) {
         if (this.next == null)
@@ -44,34 +28,13 @@ public abstract class ChainCommand {
         else
             this.next.setNext(next);
     }
-
-    public static int waitInterval = 1000;
-
     public abstract byte[] getCommand();
-
-    public static void setCam(int number)
-    {
-        if(number > 0 && number < heads.length)
-        {
-            currentAddress = heads[number-1];
-        }
-    }
 
     public void execute(String command, SerialPort serialPort) throws UnknownCommandException, SerialPortException {
 
         if (serialPort.isOpened() && command.equals(commandName)) {
             {
 
-
-            if(isDaisyChaining())
-            {
-                byte[] template = getCommand();
-                for(int i = 0 ; i < heads.length ; i++)
-                {
-                    template[0] = heads[i];
-                    serialPort.writeBytes(template);
-                }
-            }
                 byte[] template = getCommand();
                 template[0] = currentAddress;
                 serialPort.writeBytes(template);
